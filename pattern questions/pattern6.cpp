eff59b90ac6eceee8ced06c73a2f561430b90119
nik-agarwal <67484380+nik-agarwal@users.noreply.github.com>
2022-03-20 23:14:13 +0530

    Add files via upload

---
+++ pattern questions/pattern6.cpp
@@ -0,0 +1,22 @@
+// Pattern is:-
+//    1
+//    2 3
+//    3 4 5
+//    4 5 6 7
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<=4;++i){
+        for(int j=1; j<=i;++j){
+            cout<<i+j-1<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
