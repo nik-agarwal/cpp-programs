c223a0752ffc95737e285bd33fd84bd534b8f746
nik-agarwal <67484380+nik-agarwal@users.noreply.github.com>
2022-05-15 22:51:43 +0530

    Add files via upload

---
+++ dsa questions/Recursion problems/bubblesort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sortarray(int *arr,int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    for(int i=0; i<n-1;++i){
+        if(arr[i]>arr[i+1])
+          swap(arr[i],arr[i+1]);
+    }
+    sortarray(arr,n-1);
+}
+
+int main(){
+    int n=5;
+    int arr[]={2,9,4,3,7};
+    sortarray(arr,n);
+    for(int i=0; i<n;++i){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
