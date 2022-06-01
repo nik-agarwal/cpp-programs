c151d1f97c9bbdce6ba3028ce7730de8b3880457
nik-agarwal <67484380+nik-agarwal@users.noreply.github.com>
2022-06-01 23:53:19 +0530

    Update search in array (method 1).cpp

--- dsa questions/search in array (method 1).cpp
+++ dsa questions/search in array (method 1).cpp
@@ -1,7 +1,5 @@
 // Given a sorted and rotated array A of N distinct elements which is rotated at some point, and given an element key. The task is to find the index of the given element key in the array A.
-
 // Example 1:
-
 // Input:
 // N = 9
 // A[] = {5, 6, 7, 8, 9, 10, 1, 2, 3}
